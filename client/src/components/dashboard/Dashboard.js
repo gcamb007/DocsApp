9a6ef0b08263294a0622608ff8f1077d6d6f307d
James Bond <info@desgnnja.com>
2019-08-03 17:06:14 -0500

    Dashboard work

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -34,24 +34,22 @@ class Dashboard extends Component {
         </div>
 
         <div style={{ height: "50vh" }} className="container valign-wrapper">
-          <div className="row">
-            <div className="landing-copy col s12 center-align">
-              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-                onClick={this.onLogoutClick}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-                Patient Forms
-              </button>
-              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-                onClick={this.onLogoutClick}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-                Patient Records
-              </button>
-              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-                onClick={this.onLogoutClick}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-                Telemedicine
-              </button>
-            </div>
+          <div className="row col s12 landing-copy center-align">
+            <button style={{width: "160px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+              Patient Forms
+            </button>
+            <button style={{width: "160px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+              Patient Records
+            </button>
+            <button style={{width: "160px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+              Telemedicine
+            </button>
           </div>
         </div>
 
