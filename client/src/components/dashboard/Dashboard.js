78f1e816d7e6be503fdcb52908e97d393e915a66
James Bond <info@desgnnja.com>
2019-08-03 12:13:46 -0500

    Dashboard style adjustment

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -13,7 +13,7 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div style={{ height: "50vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="landing-copy col s12 center-align">
             <h4>
