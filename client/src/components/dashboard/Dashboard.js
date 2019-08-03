6516236602d31acbd78018eb5fce4aad9b485bf7
James Bond <info@desgnnja.com>
2019-08-03 16:18:14 -0500

    Dashboard work

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -29,6 +29,21 @@ class Dashboard extends Component {
             </button>
           </div>
         </div>
+        <div className="row">
+          <div className="landing-copy col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                You are logged into Dr. Santamaria's Patient Portal
+              </p>
+            </h4>
+            <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
