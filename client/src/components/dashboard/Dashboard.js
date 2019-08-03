7b340d51b4aa6312e6a8c71d7fe5e724d06acb93
James Bond <info@desgnnja.com>
2019-08-03 16:56:23 -0500

    Dashboard work

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -13,42 +13,49 @@ class Dashboard extends Component {
     const { user } = this.props.auth;
 
     return (
-      <div style={{ height: "50vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into Dr. Santamaria's Patient Portal
-              </p>
-            </h4>
-            <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-              Logout
-            </button>
+      <div>
+
+        <div style={{ height: "50vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="landing-copy col s12 center-align">
+              <h4>
+                <b>Hey there,</b> {user.name.split(" ")[0]}
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into Dr. Santamaria's Patient Portal
+                </p>
+              </h4>
+              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="landing-copy col s12 center-align">
-            <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-              Patient Forms
-            </button>
-            <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-              Patient Records
-            </button>
-            <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3">
-              Telemedicine
-            </button>
+
+        <div style={{ height: "50vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="landing-copy col s12 center-align">
+              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                Patient Forms
+              </button>
+              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                Patient Records
+              </button>
+              <button style={{width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} 
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                Telemedicine
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+
+    </div>
     );
   }
 }
