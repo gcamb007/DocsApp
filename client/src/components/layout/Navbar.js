3ee713e196d1b1f8133edeae80bce0ad41b3efaa
James Bond <info@desgnnja.com>
2019-08-03 20:00:25 -0500

    Navbar work

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white" style="height:150px letterSpacing=1.5px paddingTop=1rem background-color=black;/* color: black; */box-shadow: 0 0 12px 1px grey;">
+          <div className="nav-wrapper white">
             <a href="https://www.santamariamd.com" target="blank" className="col s4 brand-logo center grey-text text-darken-1">
               <b>Dr. Santamaria's</b> Home
             </a>
