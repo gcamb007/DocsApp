ec48bbfedff0e9e730aadeca6595d516c7e1a746
James Bond <info@desgnnja.com>
2019-08-03 19:42:03 -0500

    Navbar work

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react"; 
 
+const divStyle = {
+  color: 'blue',
+};
+
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
+      <div className="navbar-fixed" style={divStyle}>
         <nav className="z-depth-0">
           <div className="nav-wrapper white">
             <a href="https://www.santamariamd.com" target="blank" className="col s4 brand-logo center grey-text text-darken-1">
