25b41d43564e486a3983b68e80751aab005225f5
James Bond <info@desgnnja.com>
2019-08-06 18:30:34 -0500

    Register margin fix

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,8 +5,8 @@ class Navbar extends Component {
     return (
       <div className="navbar-fixed">
         <nav className="z-depth-0">
-          <div className="nav-wrapper white" style={{height:"90px", letterSpacing:"1.5px", paddingTop:"1rem", backgroundColor:"black", boxShadow:("0, 0, 12px, 1px, grey")}}>
-            <a href="https://www.santamariamd.com" target="blank" className="col s4 brand-logo center grey-text text-darken-1">
+          <div className="nav-wrapper black landing-copy col s12 center-align">
+            <a href="https://www.santamariamd.com" target="blank" className="col s12 flow-text brand-logo center white-text text-darken-1" style={{ textSize: "14px" }}>
               <b>Dr. Santamaria's</b> Home
             </a>
           </div>
