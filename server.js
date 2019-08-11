fe31246571766559d4d37faa6ae916c0ca9b66cd
James Bond <info@desgnnja.com>
2019-08-10 19:57:22 -0500

    Adding file management

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const path = require("path");
 
 const users = require("./routes/api/users");
+const file = require("./routes/api/file")
 
 const app = express();
 
@@ -36,6 +37,7 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+app.use("/api/file", file);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
