03b0c317df1479d748d08315969977c5227aae93
James Bond <info@desgnnja.com>
2019-08-11 11:00:27 -0500

    Optimizing patient archives

--- client/src/components/archives/Archives.js
+++ client/src/components/archives/Archives.js
@@ -23,7 +23,7 @@ class Archives extends Component {
 
     return (
       <div>
-        <div style={{ height: "40vh" }} className="container valign-wrapper">
+        <div style={{ height: "45vh" }} className="container valign-wrapper">
           <div className="row">
             <div className="landing-copy col s12 center-align">
               <h4>
@@ -33,38 +33,36 @@ class Archives extends Component {
                 </p>
               </h4>
               <Link to="/dashboard" className="btn-flat waves-effect">
-               <i className="material-icons left">keyboard_backspace</i> Back to portal's home
+               <i className="material-icons left">keyboard_backspace</i> 
              </Link>
             </div>
           </div>
         </div>
-        <div className="container valign-wrapper">
-          <div className="row landing-copy col s12 center-align panel panel-default">
-            <div className="panel-body">
-              <Link to="/create"  style={{margin: "1rem", width: "240px"}}
-                className="btn btn-large waves-effect waves-light hoverable red accent-3">
-                <i className="material-icons right">folder_open</i>Add a File
-              </Link>
-              <table className="table table-stripe">
-                <thead>
-                  <tr>
-                    <th>Document ID</th>
-                    <th>Document Title</th>
-                    <th>Type of Record</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.records.map(records =>
-                    <tr>
-                      <td><Link to={`/show/${records._id}`}>{records.id}</Link></td>
-                      <td>{records.title}</td>
-                      <td>{records.author}</td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </div>
+        <div>
+          <Link to="/create"  style={{margin: "1rem", width: "120px"}}
+            className="btn btn-small waves-effect waves-light hoverable blue accent-3">
+            <i className="material-icons right">add_circle_outline</i>Upload
+          </Link>
+        </div>
+        <div>
+          <table className="table table-stripe">
+            <thead>
+              <tr>
+                <th>Document ID</th>
+                <th>Document Title</th>
+                <th>Type of Record</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.records.map(records =>
+                <tr>
+                  <td><Link to={`/show/${records._id}`}>{records.id}</Link></td>
+                  <td>{records.title}</td>
+                  <td>{records.author}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
     );
